eaf64009e1aab4c96fd487389e88d8a8e66dd1e6
srinath <srinath21>
2020-07-19 01:13:47 +0530

    Initial Commit with React

---
+++ about/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Resume from 'containers/Resume/Resume'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Resume />
+      </div>
+      );
+    }
+  }
+  
+  export default App;
+  
